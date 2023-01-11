5e6d4835b708c517cd138b2064e59a79ecbc7f20
Anna Chufarova <anyatrying@gmail.com>
2023-01-11 20:25:06 +0200

    ph

---
+++ tasks/less4/task1/calculator/index.js
@@ -0,0 +1,14 @@
+export const sum = (a, b) => {
+  console.log('implementation for sum');
+};
+
+export const mult = (a, b) => {
+  console.log('implementation for mult');
+};
+
+export default (expression) => {
+  console.log('implementation for calc');
+};
+
+// экспорт по умолчанию
+// export default calc;

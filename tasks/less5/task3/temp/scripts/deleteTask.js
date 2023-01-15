d6b29d59cb84da434fb1fce4c62e059d201ab45c
Anna Chufarova <anyatrying@gmail.com>
2023-01-15 21:16:15 +0200

    ph

---
+++ tasks/less5/task3/temp/scripts/deleteTask.js
@@ -0,0 +1,17 @@
+import { renderTasks } from './renderer.js';
+import { setItem } from './storage.js';
+import { deleteTask, getTasksList } from './tasksGateway.js';
+export const onDeleteTask = e => {
+  const isDelete = e.target.classList.contains('list__item__delete-btn');
+  if (!isDelete) {
+    return;
+  }
+  const id = e.target.dataset.id;
+  const listElem = document.querySelector('.list__item');
+  const deleteBtn = document.querySelector('.list__item__delete-btn');
+  deleteTask(id);
+  if (id === undefined) {
+    return listElem.remove();
+  }
+  deleteBtn.addEventListener('click', deleteTask);
+};

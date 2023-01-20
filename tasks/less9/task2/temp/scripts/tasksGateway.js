87f4e42f694c696fb6b15efcb071922674eef102
Anna Chufarova <anyatrying@gmail.com>
2023-01-20 15:11:30 +0200

    ph

---
+++ tasks/less9/task2/temp/scripts/tasksGateway.js
@@ -0,0 +1,46 @@
+import "core-js/modules/es.promise.js";
+import "core-js/modules/es.symbol.description.js";
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return typeof key === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (typeof input !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (typeof res !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
+const baseUrl = "https://63a8af9c100b7737b983b748.mockapi.io/api/v1/task";
+const mapTasks = tasks => {
+  tasks.map(function (_id) {
+    for (var _len = arguments.length, rest = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+      rest[_key - 1] = arguments[_key];
+    }
+    return _objectSpread({
+      id: rest.id
+    }, rest);
+  });
+};
+export const getTasksList = () => {
+  return fetch(baseUrl).then(response => response.json()).then(tasks => tasks.map(task => _objectSpread(_objectSpread({}, task), {}, {
+    id: task.id
+  })));
+};
+export const createTask = taskData => {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8"
+    },
+    body: JSON.stringify(taskData)
+  });
+};
+export const updateTask = (taskId, updatedTaskData) => {
+  return fetch("".concat(baseUrl, "/").concat(taskId), {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8"
+    },
+    body: JSON.stringify(updatedTaskData)
+  });
+};
+export const deleteTask = id => {
+  return fetch("".concat(baseUrl, "/").concat(id), {
+    method: "DELETE"
+  });
+};
